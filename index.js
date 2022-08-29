a6013e3c1f738fb9780dcb98e42bcb37854efe1f
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-08-29 12:51:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  // Write your algorithm here 
+  // MY ALGORITHM
+  // declare isPalindrome function with word as the parameter
+  // split the word argument into an array using the split method 
+  const splitString = word.split('');
+  console.log(splitString)
+  // reverse the splitted word using the reverse method 
+  const reverseString = splitString.reverse();
+  console.log(reverseString)
+  // join the reversed word into a string 
+  const joinString = reverseString.join('');
+  console.log(joinString)
+  // use an if statement to see if the reversed string is equal to the entered argument and // return true or false 
+  return joinString === word
+  }
+  isPalindrome('abba')
 
 /* 
   Add your pseudocode here
@@ -8,6 +22,10 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  I have taken that word, split it into individual letters, reversed the letters
+  and then joined the letters into a word. 
+  I have then used an if statement to compare if the reversed word is the 
+  same as the initial word
 */
 
 // You can run `node index.js` to view these console logs
